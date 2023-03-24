63d6719a72bf619a6738581f2f38b1af17af946e
Joseph Kanaan <josega572@gmail.com>
2023-03-24 14:02:31 -0500

    added Rakdos Burn

--- commander-picker.js
+++ commander-picker.js
@@ -7,7 +7,7 @@ function getRandomInt(max) {
 
 let josephDecks = [{id:1, commander: 'Lathril'}, {id:2, commander: 'Chishiro'}, {id: 3, commander: 'Kyler (J)'}, {id: 4, commander: 'Rocco'}, {id: 5, commander: 'Animar'}, {id: 6, commander: 'Rogues'}, {id: 7, commander: 'Abbadon'}]
 
-let nickDecks = [{id:1, commander: 'Kyler (N)'}, {id:2, commander: 'Elenda'}, {id: 3, commander: 'Brago'}, {id: 4, commander: 'Equipment'}, {id: 5, commander: 'Rakdos'}, {id: 6, commander: 'Poison'}, {id: 7, commander: 'Athreos'}, {id: 8, commander: 'Neyali'}]
+let nickDecks = [{id:1, commander: 'Kyler (N)'}, {id:2, commander: 'Elenda'}, {id: 3, commander: 'Brago'}, {id: 4, commander: 'Equipment'}, {id: 5, commander: 'Rakdos'}, {id: 6, commander: 'Poison'}, {id: 7, commander: 'Athreos'}, {id: 8, commander: 'Neyali'}, {id: 9, 'Rakdos Burn'}]
 
 let kelbyDecks = [{id: 1, commander: 'Milicent'}, {id: 2, commander: 'Gyome'}, {id: 3, commander: 'Necrons'}, {id: 4, commander: 'Kardur'}, {id: 5, commander: 'Isperia'}]
 
